d5630a4a7c4e86b7f3b4852bde919c8ffef0be80
Panda-Abhisek <abhisekpanda114@gmail.com>
2026-03-17 18:33:33 +0530

    feat(theme): integrate mode-toggle into app layout
    - Add ModeToggle at bottom-center of the page
    - Wrap content with BackgroundLines component
    - Use semantic main element with proper z-index

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import WaitingPage from "./pages/waiting-page"
+import { BackgroundLines } from "@/components/ui/background-lines"
+import ModeToggle from "@/components/mode-toggle"
+import WaitingPage from "@/pages/waiting-page"
 
 function App() {
-  return <WaitingPage />
+  return (
+    <BackgroundLines>
+      <main className="relative min-h-screen">
+        <WaitingPage />
+        <div className="fixed bottom-8 left-1/2 -translate-x-1/2 z-50">
+          <ModeToggle />
+        </div>
+      </main>
+    </BackgroundLines>
+  )
 }
 
 export default App
