c7ca7bf20f32974428fb74c9f4f131b9685b0d63
Panda-Abhisek <abhisekpanda114@gmail.com>
2026-03-17 23:16:32 +0530

    refactor: simplify layout structure and fix background height
    - Merge Home.jsx into App.jsx for cleaner component tree
    - Fix BackgroundLines to use full viewport height (h-full min-h-screen)
    - Add responsive positioning for ModeToggle (bottom-6 sm:bottom-8)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BackgroundLines } from "@/components/ui/background-lines"
+import { HomePage } from "@/components/home-page"
 import ModeToggle from "@/components/mode-toggle"
-import Home from "./pages/Home"
 
 function App() {
   return (
     <BackgroundLines>
       <main className="relative min-h-screen">
-        <Home />
-        <div className="fixed bottom-8 left-1/2 -translate-x-1/2 z-50">
+        <HomePage />
+        <div className="fixed bottom-6 sm:bottom-8 left-1/2 -translate-x-1/2 z-50">
           <ModeToggle />
         </div>
       </main>
